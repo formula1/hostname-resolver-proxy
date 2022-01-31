684811761821849b01caf5fa4861dd560d34f487
Samu <samtobia@gmail.com>
2022-01-30 23:23:26 -0800

    Code Feat: setting iup greenlock

---
+++ src/init-greenlock.ts
@@ -0,0 +1,28 @@
+
+
+import { init as greenlockInit, Servers as GreenlockServers } from "greenlock-express";
+import {
+  ProxyConfig
+} from "domain-config-formatter"
+import { GreenlockConfig } from "domain-config-formatter/src/types/greenlock";
+
+export type TypeInitGreenlockArg = {
+  maintainerEmail: string,
+  greenlockDir: string,
+  packageDir: string,
+}
+
+export async function initGreenlock(
+  { maintainerEmail, greenlockDir, packageDir }: TypeInitArg
+): Promise<GreenlockServers>{
+  return await new Promise((res)=>{
+    greenlockInit({
+      packageRoot: packageDir,
+      configDir: greenlockDir,
+      maintainerEmail: maintainerEmail,
+      cluster: false
+    }).ready((glx)=>{
+      res(glx)
+    })
+  })
+}
